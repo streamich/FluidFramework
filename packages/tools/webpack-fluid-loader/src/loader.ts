e184799fe297e643a8ef6ae0dabb9444cba05755
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-04-06 20:51:36 +0000

    port 7070 part 2

--- packages/tools/webpack-fluid-loader/src/loader.ts
+++ packages/tools/webpack-fluid-loader/src/loader.ts
@@ -63,6 +63,7 @@ export interface IRouterliciousRouteOptions extends IBaseRouteOptions {
 export interface ITinyliciousRouteOptions extends IBaseRouteOptions {
     mode: "tinylicious";
     bearerSecret?: string;
+    tinyliciousPort?: number;
 }
 
 export interface IOdspRouteOptions extends IBaseRouteOptions {
